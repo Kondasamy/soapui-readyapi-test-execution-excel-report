214801cd7c37f939bb833892bbb6d8f8cad09d76
Kondasamy Jayaraman <jkondasamy1992@gmail.com>
2016-08-25 20:56:04 +0530

    Processed all the handlings of result export and freezing the version 1.0

--- src/main/groovy/com/kondasamy/soapui/plugin/ProjectExportTestReport.groovy
+++ src/main/groovy/com/kondasamy/soapui/plugin/ProjectExportTestReport.groovy
@@ -7,7 +7,6 @@ package com.kondasamy.soapui.plugin
 
 import com.eviware.soapui.SoapUI
 import com.eviware.soapui.model.project.Project
-import com.eviware.soapui.impl.wsdl.WsdlProject;
 import com.eviware.soapui.plugins.ActionConfiguration
 import com.eviware.soapui.support.UISupport
 import com.eviware.soapui.support.action.support.AbstractSoapUIAction
@@ -42,8 +41,9 @@ class ProjectExportTestReport extends AbstractSoapUIAction <Project>
             SubDir1.mkdirs()
             file = new File(SubDir1,fileName)
         }
-        //Append header in CSV file
-        file.append("PROJECT,TYPE,TEST CASE NAME,TEST STEP COUNT,TEST STATUS,EXECUTED BY\n")
+        if(file.length()==0)
+            file.append("PROJECT,TYPE,TEST CASE NAME,TEST STEP COUNT,TEST STATUS,EXECUTED BY\n")
+
         /*
          * Iterate through each test cases and collect the data
          */
@@ -62,48 +62,104 @@ class ProjectExportTestReport extends AbstractSoapUIAction <Project>
                                             {
                                                 def tcName = testCase.name
                                                 def testStepCount = testCase.testStepCount
-                                                def testRunFlag = 0
+                                                def testCasePassStatusFlag =0
+                                                def testCaseFailStatusFlag =0
+                                                String testCaseStatus
                                                 testCase.getTestStepsOfType(com.eviware.soapui.impl.wsdl.teststeps.RestTestRequestStep.class).each
                                                         {
-                                                            tests->
-                                                                if(!tests.isDisabled() && tests.httpRequest.response!=null)
+                                                            testRequestStep->
+                                                                if(!testRequestStep.isDisabled() && testRequestStep.httpRequest.response!=null)
                                                                 {
-                                                                    def response = tests.httpRequest.response.getRawResponseData()
+                                                                    String testStepStatus
+                                                                    def response = testRequestStep.httpRequest.response.getRawResponseData()
                                                                     if( response == null || response.length == 0 )
                                                                     {
-                                                                        SoapUI.log.warn "Empty Response data for TestStep : "+ tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
+                                                                        SoapUI.log.warn "Empty Response data for TestStep : "+ testRequestStep.testStep.testCase.testSuite.name + "=>"+testRequestStep.testStep.testCase.name+ "->"+testRequestStep.name
                                                                     }
                                                                     else
                                                                     {
-                                                                        def tstName = tests.getName()
-                                                                        SoapUI.log.info "Assertion Status: "+tests.assertionStatus +"Test Step : " +tstName
+                                                                        def tstName = testRequestStep.getName()
+                                                                        //Loop through each assertion and conclude the test step status
+                                                                        def assertionPassStatusFlag = 0
+                                                                        def assertionFailStatusFlag = 0
+                                                                        testRequestStep.assertionList.each
+                                                                                {
+                                                                                    assertion->
+                                                                                        if(assertion.status.toString().equalsIgnoreCase("VALID"))
+                                                                                            assertionPassStatusFlag++
+                                                                                        else if(assertion.status.toString().equalsIgnoreCase("FAILED"))
+                                                                                            assertionFailStatusFlag++
+                                                                                }
+                                                                        if(assertionPassStatusFlag>0 && assertionFailStatusFlag==0)
+                                                                            testStepStatus = "PASS"
+                                                                        else if(assertionFailStatusFlag>0)
+                                                                            testStepStatus = "FAIL"
+                                                                        else
+                                                                            testStepStatus = "NO RUN"
+//                                                                        SoapUI.log.info "Test case status: " + testStepStatus + " Test case : " + tcName
                                                                     }
+
+                                                                    //Loop through the test step status and conclude the test case status
+                                                                    if(testStepStatus.equalsIgnoreCase("PASS"))
+                                                                        testCasePassStatusFlag++
+                                                                    else if (testStepStatus.equalsIgnoreCase("FAIL"))
+                                                                        testCaseFailStatusFlag++
                                                                 }
                                                         }
                                                 testCase.getTestStepsOfType(com.eviware.soapui.impl.wsdl.teststeps.WsdlTestRequestStep.class).each
                                                         {
-                                                            tests->
-                                                                if(!tests.isDisabled() && tests.httpRequest.response!=null)
+                                                            testRequestStep ->
+                                                                if (!testRequestStep.isDisabled() && testRequestStep.httpRequest.response != null)
                                                                 {
-                                                                    def response = tests.httpRequest.response.getRawResponseData()
-                                                                    if( response == null || response.length == 0 )
+                                                                    String testStepStatus
+                                                                    def response = testRequestStep.httpRequest.response.getRawResponseData()
+                                                                    if (response == null || response.length == 0)
                                                                     {
-                                                                        SoapUI.log.warn "Empty Response data for TestStep : "+ tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
+                                                                        //SoapUI.log.warn "Empty Response data for TestStep : " + testRequestStep.testStep.testCase.testSuite.name + "=>" + testRequestStep.testStep.testCase.name + "->" + testRequestStep.name
                                                                     }
                                                                     else
                                                                     {
-                                                                        def tstName = tests.getName()
-                                                                        SoapUI.log.info "Assertion Status: "+tests.assertionStatus +"Test Step : " +tstName
+                                                                        def tstName = testRequestStep.getName()
+                                                                        //Loop through each assertion and conclude the test step status
+                                                                        def assertionPassStatusFlag = 0
+                                                                        def assertionFailStatusFlag = 0
+                                                                        testRequestStep.assertionList.each
+                                                                                {
+                                                                                    assertion ->
+                                                                                        if (assertion.status.toString().equalsIgnoreCase("VALID"))
+                                                                                            assertionPassStatusFlag++
+                                                                                        else if (assertion.status.toString().equalsIgnoreCase("FAILED"))
+                                                                                            assertionFailStatusFlag++
+                                                                                }
+                                                                        if (assertionPassStatusFlag > 0 && assertionFailStatusFlag == 0)
+                                                                            testStepStatus = "PASS"
+                                                                        else if (assertionFailStatusFlag > 0)
+                                                                            testStepStatus = "FAIL"
+                                                                        else
+                                                                            testStepStatus = "NO RUN"
+//                                                                        SoapUI.log.info "Test case status: " + testStepStatus + " Test case : " + tcName
                                                                     }
+
+                                                                    //Loop through the test step status and conclude the test case status
+                                                                    if (testStepStatus.equalsIgnoreCase("PASS"))
+                                                                        testCasePassStatusFlag++
+                                                                    else if (testStepStatus.equalsIgnoreCase("FAIL"))
+                                                                        testCaseFailStatusFlag++
                                                                 }
                                                         }
-                                                SoapUI.log.info "Test Case Name : "+ tcName
-                                                file.append("$projName,$tsName,$tcName,$testStepCount,PASS,$currentUser\n")
+                                                if(testCasePassStatusFlag>0 && testCaseFailStatusFlag<=0)
+                                                    testCaseStatus="PASS"
+                                                else if (testCaseFailStatusFlag>0)
+                                                    testCaseStatus="FAIL"
+                                                else
+                                                    testCaseStatus="NO RUN"
+
+                                                //SoapUI.log.info "Test Case Name : "+ tcName
+                                                if(testCaseStatus.equalsIgnoreCase("PASS")||testCaseStatus.equalsIgnoreCase("FAIL"))
+                                                    file.append("$projName,$tsName,$tcName,$testStepCount,$testCaseStatus,$currentUser\n")
                                             }
                                     }
                         }
-
-
                 }
         UISupport.showInfoMessage("Artifacts Successfully exported!! Please see the SoapUI log for more information!","File Export Success!!!")
     }
