b48dcb86b97babce3b7b31ad081105cd29e6d633
Kondasamy Jayaraman <jkondasamy1992@gmail.com>
2016-08-25 17:55:38 +0530

    Adding support for test case wise iteration test and commented unnecessary Maven information in POM.xml

--- src/main/groovy/com/kondasamy/soapui/plugin/ProjectExportTestReport.groovy
+++ src/main/groovy/com/kondasamy/soapui/plugin/ProjectExportTestReport.groovy
@@ -26,19 +26,26 @@ class ProjectExportTestReport extends AbstractSoapUIAction <Project>
         /*
          * Create CSV file in the User directory
          */
+        def currentUser = System.getProperty('user.name')
         def today= new Date().format("yyyyMMdd")
         def fileName = today+"_SoapUI_Test_Results.csv"
         def mainDir = System.getProperty('user.home')
         def SubDir = "\\SoapUI Data\\"
         def SubDir1 = new File(mainDir,SubDir)
+        def file
         if(SubDir1.exists())
         {
-            def file = new File(SubDir1,fileName)
+            file = new File(SubDir1,fileName)
         }
         else
         {
             SubDir1.mkdirs()
-            def file = new File(SubDir1,fileName)
+            file = new File(SubDir1,fileName)
+        }
+        //Append header in CSV file
+        if(file.exists())
+        {
+            file.append("PROJECT,TYPE,TEST CASE NAME,TEST STEP COUNT,TEST STATUS,EXECUTED BY\n")
         }
         /*
          * Iterate through each test cases and collect the data
@@ -52,53 +59,39 @@ class ProjectExportTestReport extends AbstractSoapUIAction <Project>
                                 {
                                     testCase->
                                         def tcName = testCase.name
+                                        def testStepCount = testCase.testStepCount
+                                        def testRunFlag = 0
                                         testCase.getTestStepsOfType(com.eviware.soapui.impl.wsdl.teststeps.RestTestRequestStep.class).each
                                                 {
                                                     tests->
-                                                        def response = tests.httpRequest.response
-                                                        if( response == null )
-                                                        {
-                                                            SoapUI.log.warn "Missing Response for TestStep : " + tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
-                                                        }
-                                                        def data = response.getRawResponseData()
-                                                        if( data == null || data.length == 0 )
+                                                        def response = tests.httpRequest.response.getRawResponseData()
+                                                        if( response == null || response.length == 0 )
                                                         {
                                                             SoapUI.log.warn "Empty Response data for TestStep : "+ tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
                                                         }
                                                         else
                                                         {
                                                             def tstName = tests.getName()
-                                                            SoapUI.log.info "***Raw Request and Raw Response is exported to a file :: ==>"+mainDir+ SubDir+"\\"+fileName
-
+                                                            SoapUI.log.info "Assertion Status: "+tests.assertionStatus +"Test Step : " +tstName
                                                         }
-
                                                 }
                                         testCase.getTestStepsOfType(com.eviware.soapui.impl.wsdl.teststeps.WsdlTestRequestStep.class).each
                                                 {
                                                     tests->
-                                                        def response = tests.httpRequest.response
-                                                        if( response == null )
-                                                        {
-                                                            SoapUI.log.warn "Missing Response for TestStep : " + tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
-                                                            return
-                                                        }
-                                                        def data = response.getRawResponseData()
-                                                        if( data == null || data.length == 0 )
+                                                        def response = tests.httpRequest.response.getRawResponseData()
+                                                        if( response == null || response.length == 0 )
                                                         {
                                                             SoapUI.log.warn "Empty Response data for TestStep : "+ tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
-                                                            return
                                                         }
                                                         else
                                                         {
-                                                            def rawRequest = new String(response.getRawRequestData(),"UTF-8")
-                                                            def rawResponse = new String(response.getRawResponseData(),"UTF-8")
                                                             def tstName = tests.getName()
-
-
-
+                                                            SoapUI.log.info "Assertion Status: "+tests.assertionStatus +"Test Step : " +tstName
                                                         }
 
                                                 }
+                                        SoapUI.log.info "Test Case Name : "+ tcName
+                                        file.append("$projName,$tsName,$tcName,$testStepCount,PASS,$currentUser")
                                 }
 
                 }
--- src/main/groovy/com/kondasamy/soapui/plugin/Utilities/ExcelUtility.groovy
+++
@@ -1,7 +0,0 @@
-package com.kondasamy.soapui.plugin.Utilities
-
-import com.xlson.groovycsv.*
-
-def f = new File("D:/DUMMY.csv")
-def someText = "aghjg,ghgjhg,564\n"
-f.append(someText)
