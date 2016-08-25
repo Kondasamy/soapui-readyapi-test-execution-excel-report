bf888b97ad29f73a973b8a015eed97dbaf73739b
Kondasamy Jayaraman <jkondasamy1992@gmail.com>
2016-08-25 15:47:56 +0530

    Adding the code for below functionality, CSV file creation Test case wise iteration Export to CSV file

--- src/main/groovy/com/kondasamy/soapui/plugin/ProjectExportTestReport.groovy
+++ src/main/groovy/com/kondasamy/soapui/plugin/ProjectExportTestReport.groovy
@@ -17,18 +17,41 @@ class ProjectExportTestReport extends AbstractSoapUIAction <Project>
 {
     public ProjectExportTestReport()
     {
-        super("Plugin:Export Test Report", "Exports test execution report to an Excel(XLSX) file")
+        super("Plugin:Export Test Report", "Exports test execution report to a CSV file")
     }
 
     @Override
     void perform(Project project, Object o)
     {
+        /*
+         * Create CSV file in the User directory
+         */
+        def today= new Date().format("yyyyMMdd")
+        def fileName = today+"_SoapUI_Test_Results.csv"
+        def mainDir = System.getProperty('user.home')
+        def SubDir = "\\SoapUI Data\\"
+        def SubDir1 = new File(mainDir,SubDir)
+        if(SubDir1.exists())
+        {
+            def file = new File(SubDir1,fileName)
+        }
+        else
+        {
+            SubDir1.mkdirs()
+            def file = new File(SubDir1,fileName)
+        }
+        /*
+         * Iterate through each test cases and collect the data
+         */
+        def projName = project.name
         project.getTestSuiteList().each
                 {
                     testSuite->
+                        def tsName = testSuite.name
                         testSuite.getTestCaseList().each
                                 {
                                     testCase->
+                                        def tcName = testCase.name
                                         testCase.getTestStepsOfType(com.eviware.soapui.impl.wsdl.teststeps.RestTestRequestStep.class).each
                                                 {
                                                     tests->
@@ -36,57 +59,16 @@ class ProjectExportTestReport extends AbstractSoapUIAction <Project>
                                                         if( response == null )
                                                         {
                                                             SoapUI.log.warn "Missing Response for TestStep : " + tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
-                                                            return
                                                         }
                                                         def data = response.getRawResponseData()
                                                         if( data == null || data.length == 0 )
                                                         {
                                                             SoapUI.log.warn "Empty Response data for TestStep : "+ tests.testStep.testCase.testSuite.name + "=>"+tests.testStep.testCase.name+ "->"+tests.name
-                                                            return
                                                         }
                                                         else
                                                         {
-                                                            def rawRequest = new String(response.getRawRequestData(),"UTF-8")
-                                                            def rawResponse = new String(response.getRawResponseData(),"UTF-8")
                                                             def tstName = tests.getName()
-                                                            def tcName = tests.testCase.getName()
-                                                            def tsName = tests.testCase.testSuite.getName()
-                                                            def projName = tests.testCase.testSuite.project.name
-
-                                                            def today= new Date()
-                                                            String today1 = today.format("yyyyMMdd-HH:mm:ss.S")
-                                                            String result = today1.replaceAll(":", "");
-                                                            String result1 = result.replaceAll("-", "");
-                                                            String fileName = tcName+"__" +tstName+ "__" + result1 +".txt"
-
-//                                                            String fileName1 = fileName.replaceAll("/", "-");
-//                                                            String fileName2 = fileName1.replaceAll(";","-");
-//                                                            String fileName3 = fileName2.replaceAll(":","-");
-//                                                            String fileName4 = fileName3.replaceAll(",","-");
-//                                                            String fileName5 = fileName4.replaceAll("\\?","-");
-//                                                            String fileName6 = fileName5.replaceAll("-","_")
-                                                            String fileName6 = fileName.replaceAll("[^a-zA-Z0-9.-]", "_")
-
-                                                            def mainDir = System.getProperty('user.home')
-//                                                            def SubDir = "\\SoapUI Data\\"+projName+"\\"+tsName
-                                                            def SubDir = "\\SoapUI Data\\"+projName
-                                                            def SubDir1 = new File(mainDir,SubDir)
-                                                            //SubDir1.mkdirs()
-                                                            if(SubDir1.exists())
-                                                            {
-                                                                def file = new File(SubDir1,fileName6)
-                                                                if(!file.exists())
-                                                                    file.append "Raw Request:" + System.getProperty("line.separator") + rawRequest+System.getProperty("line.separator")+System.getProperty("line.separator") + "Raw Response :"+ System.getProperty("line.separator")+ rawResponse
-                                                                else
-                                                                    file << "Raw Request:" + System.getProperty("line.separator") + rawRequest+System.getProperty("line.separator")+System.getProperty("line.separator") + "Raw Response :"+ System.getProperty("line.separator")+ rawResponse
-                                                            }
-                                                            else
-                                                            {
-                                                                SubDir1.mkdirs()
-                                                                def file = new File(SubDir1,fileName6)
-                                                                file << "Raw Request:" + System.getProperty("line.separator") +  rawRequest+System.getProperty("line.separator")+System.getProperty("line.separator") + "Raw Response :"+ System.getProperty("line.separator")+ rawResponse
-                                                            }
-                                                            SoapUI.log.info "***Raw Request and Raw Response is exported to a file :: ==>"+mainDir+ SubDir+"\\"+fileName6
+                                                            SoapUI.log.info "***Raw Request and Raw Response is exported to a file :: ==>"+mainDir+ SubDir+"\\"+fileName
 
                                                         }
 
@@ -111,43 +93,8 @@ class ProjectExportTestReport extends AbstractSoapUIAction <Project>
                                                             def rawRequest = new String(response.getRawRequestData(),"UTF-8")
                                                             def rawResponse = new String(response.getRawResponseData(),"UTF-8")
                                                             def tstName = tests.getName()
-                                                            def tcName = tests.testCase.getName()
-                                                            def tsName = tests.testCase.testSuite.getName()
-                                                            def projName = tests.testCase.testSuite.project.name
 
-                                                            def today= new Date()
-                                                            String today1 = today.format("yyyyMMdd-HH:mm:ss.S")
-                                                            String result = today1.replaceAll(":", "");
-                                                            String result1 = result.replaceAll("-", "");
-                                                            String fileName = tcName+"__" +tstName+ "__" + result1 +".txt"
 
-//                                                            String fileName1 = fileName.replaceAll("/", "-");
-//                                                            String fileName2 = fileName1.replaceAll(";","-");
-//                                                            String fileName3 = fileName2.replaceAll(":","-");
-//                                                            String fileName4 = fileName3.replaceAll(",","-");
-//                                                            String fileName5 = fileName4.replaceAll("\\?","-");
-//                                                            String fileName6 = fileName5.replaceAll("-","_")
-                                                            String fileName6 = fileName.replaceAll("[^a-zA-Z0-9.-]", "_")
-
-                                                            def mainDir = System.getProperty('user.home')
-                                                            def SubDir = "\\SoapUI Data\\"+projName
-                                                            def SubDir1 = new File(mainDir,SubDir)
-                                                            //SubDir1.mkdirs()
-                                                            if(SubDir1.exists())
-                                                            {
-                                                                def file = new File(SubDir1,fileName6)
-                                                                if(!file.exists())
-                                                                    file.append "Raw Request:" + System.getProperty("line.separator") + rawRequest+System.getProperty("line.separator")+System.getProperty("line.separator") + "Raw Response :"+ System.getProperty("line.separator")+ rawResponse
-                                                                else
-                                                                    file << "Raw Request:" + System.getProperty("line.separator") + rawRequest+System.getProperty("line.separator")+System.getProperty("line.separator") + "Raw Response :"+ System.getProperty("line.separator")+ rawResponse
-                                                            }
-                                                            else
-                                                            {
-                                                                SubDir1.mkdirs()
-                                                                def file = new File(SubDir1,fileName6)
-                                                                file << "Raw Request:" + System.getProperty("line.separator") + rawRequest+System.getProperty("line.separator")+System.getProperty("line.separator") + "Raw Response :"+ System.getProperty("line.separator")+ rawResponse
-                                                            }
-                                                            SoapUI.log.info "**Raw Request and Raw Response is exported to a file :: ==>"+mainDir+ SubDir+"\\"+fileName6
 
                                                         }
 
--- src/main/groovy/com/kondasamy/soapui/plugin/Utilities/ExcelUtility.groovy
+++ src/main/groovy/com/kondasamy/soapui/plugin/Utilities/ExcelUtility.groovy
@@ -3,7 +3,5 @@ package com.kondasamy.soapui.plugin.Utilities
 import com.xlson.groovycsv.*
 
 def f = new File("D:/DUMMY.csv")
-def someText = '''abcd,2454,6786
-bsgc,8989,6565
-fgf,8989,323'''
+def someText = "aghjg,ghgjhg,564\n"
 f.append(someText)
